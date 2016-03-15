be92a24c4019bfa599f24a7f065634d46f8ad2c3
Julio <jcadriaz@uc.cl>
2016-03-15 15:14:40 +0100

    little fix

--- api/services/SQLiteProcessor.js
+++ api/services/SQLiteProcessor.js
@@ -31,8 +31,8 @@ module.exports = {
 							FROM session  a
 							LEFT JOIN session b
 								ON a.timestamp < b.timestamp
-								AND b.event IN ('pause','stop')
-							WHERE a.event IN ('start','restart')
+								AND b.event IN ('pause','stop','autostop','suspend')
+							WHERE a.event IN ('start','restart','autorestart','resume')
 							GROUP BY a.timestamp,a.event
 						) a`;
 
